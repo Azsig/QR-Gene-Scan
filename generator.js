18520796f4bd943a32f66a5101cf1273b3b29c41
Azsig <yasir_abdulaziz@yahoo.co.id>
2024-01-27 19:56:20 +0700

    change generator js, make download work on mobile

--- generator.js
+++ generator.js
@@ -8,27 +8,36 @@ const downloadBtn = generatorDiv.querySelector('.generator-btn .gene-link');
 
 let qrImage = new QRCode(document.querySelector('.img'))
 
+let img;
+
 function makeCode(){
     let qrValue = document.querySelector('.generator-form input').value;
     if(!qrValue.trim()) return;
 
     qrImage.makeCode(qrValue);
     document.querySelector('.generator').classList.add('active');
+
+    img = generatorDiv.querySelector('.img img');
+    console.log(img);
+
+    setTimeout(link,1000)
+
+    
+}
+
+function link(){
+    imgURL = img.getAttribute('src');
+    console.log(imgURL);
+    if(!imgURL) return;
+    fetchImage(imgURL);
 }
 
 const generateBtn = document.querySelector(".generator-form button")
 
 generateBtn.addEventListener('click', makeCode)
 
-const img = generatorDiv.querySelector('.img img');
+ 
 
-downloadBtn.addEventListener('click', () => {
-    imgURL = img.src;
-    console.log(imgURL);
-    if(!imgURL) return;
-    fetchImage(imgURL);
-    
-})
 
 function fetchImage(url){
     fetch(url).then(res => res.blob()).then(file => {
@@ -41,11 +50,8 @@ function fetchImage(url){
 }
 
 function download(tempFIle, file_name, extension){
-    let a = document.createElement('a');
+    let a = document.querySelector('.generator-btn a');
     a.href = tempFIle;
     a.download = `${file_name}.${extension}`;
-    document.body.appendChild(a);
-    a.click();
-    a.remove();
 }
 
